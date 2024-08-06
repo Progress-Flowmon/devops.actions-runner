3aafb607d5aa0a1092a64db6a8ab41f201c01a3b
eric sciple <ericsciple@users.noreply.github.com>
2024-08-06 09:49:35 -0700

    Add .NET OS compatibility test

--- src/Runner.Worker/OSWarningChecker.cs
+++ src/Runner.Worker/OSWarningChecker.cs
@@ -16,17 +16,6 @@ namespace GitHub.Runner.Worker
         Task CheckOSAsync(IExecutionContext context, IList<OSWarning> osWarnings);
     }
 
-#if OS_WINDOWS || OS_OSX
-    public sealed class OSWarningChecker : RunnerService, IOSWarningChecker
-    {
-        public Task CheckOSAsync(IExecutionContext context, IList<OSWarning> osWarnings)
-        {
-            ArgUtil.NotNull(context, nameof(context));
-            ArgUtil.NotNull(osWarnings, nameof(osWarnings));
-            return Task.CompletedTask;
-        }
-    }
-#else
     public sealed class OSWarningChecker : RunnerService, IOSWarningChecker
     {
         private static readonly TimeSpan s_matchTimeout = TimeSpan.FromMilliseconds(100);
@@ -36,6 +25,17 @@ namespace GitHub.Runner.Worker
         {
             ArgUtil.NotNull(context, nameof(context));
             ArgUtil.NotNull(osWarnings, nameof(osWarnings));
+            context.Output("Testing runner upgrade compatibility");
+            var isCompatible = true;
+#if !OS_WINDOWS && !OS_OSX
+            isCompatible = await TestMatchFilesAsync(context, osWarnings);
+#endif
+            await CheckDotNet8CompatibilityAsync(context, omitAnnotation: !isCompatible);
+        }
+
+#if !OS_WINDOWS && !OS_OSX
+        public async Task<bool> TestMatchFilesAsync(IExecutionContext context, IList<OSWarning> osWarnings)
+        {
             foreach (var osWarning in osWarnings)
             {
                 if (string.IsNullOrEmpty(osWarning.FilePath))
@@ -68,7 +68,7 @@ namespace GitHub.Runner.Worker
                             {
                                 context.Warning(osWarning.Warning);
                                 context.Global.JobTelemetry.Add(new JobTelemetry() { Type = JobTelemetryType.General, Message = $"OS warning: {osWarning.Warning}" });
-                                return;
+                                return false;
                             }
                         }
                     }
@@ -80,8 +80,70 @@ namespace GitHub.Runner.Worker
                     context.Global.JobTelemetry.Add(new JobTelemetry() { Type = JobTelemetryType.General, Message = $"An error occurred while checking OS warnings for file '{osWarning.FilePath}' and regex '{osWarning.RegularExpression}': {ex.Message}" });
                 }
             }
+
+            return true;
+        }
+#endif
+
+        private async Task CheckDotNet8CompatibilityAsync(IExecutionContext context, bool omitAnnotation)
+        {
+            try
+            {
+                List<string> output = new();
+                object outputLock = new();
+                using (var process = HostContext.CreateService<IProcessInvoker>())
+                {
+                    process.OutputDataReceived += delegate (object sender, ProcessDataReceivedEventArgs stdout)
+                    {
+                        if (!string.IsNullOrEmpty(stdout.Data))
+                        {
+                            lock (outputLock)
+                            {
+                                output.Add(stdout.Data);
+                                Trace.Info(stdout.Data);
+                            }
+                        }
+                    };
+
+                    process.ErrorDataReceived += delegate (object sender, ProcessDataReceivedEventArgs stderr)
+                    {
+                        if (!string.IsNullOrEmpty(stderr.Data))
+                        {
+                            lock (outputLock)
+                            {
+                                output.Add(stderr.Data);
+                                Trace.Error(stderr.Data);
+                            }
+                        }
+                    };
+
+                    int exitCode = await process.ExecuteAsync(
+                        workingDirectory: HostContext.GetDirectory(WellKnownDirectory.Root),
+                        fileName: Path.Combine(HostContext.GetDirectory(WellKnownDirectory.Bin), "testDotNet8Compatibility", $"TestDotNet8Compatibility{IOUtil.ExeExtension}"),
+                        arguments: string.Empty,
+                        environment: null,
+                        cancellationToken: context.CancellationToken);
+
+                    var outputStr = string.Join("\n", output).Trim();
+                    if (exitCode != 0 || !string.Equals(outputStr, "Hello from .NET 8!", StringComparison.Ordinal))
+                    {
+                        if (!omitAnnotation)
+                        {
+                            context.Warning("The runner is not compatible with .NET 8.");
+                        }
+
+                        var shortOutput = outputStr.Length > 200 ? string.Concat(outputStr.Substring(0, 200), "[...]") : outputStr;
+                        context.Global.JobTelemetry.Add(new JobTelemetry() { Type = JobTelemetryType.General, Message = $".NET 8 OS compatibility test failed with exit code '{exitCode}' and output: {shortOutput}" });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.Error("An error occurred while testing .NET 8 compatibility'");
+                Trace.Error(ex);
+                context.Global.JobTelemetry.Add(new JobTelemetry() { Type = JobTelemetryType.General, Message = $"An error occurred while testing .NET 8 compatibility; exception type '{ex.GetType().FullName}'; message: {ex.Message}" });
+            }
         }
     }
-#endif
 }
 
---
+++ src/TestDotNet8Compatibility/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace TestDotNet8Compatibility
+{
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            Console.WriteLine("Hello from .NET 8!");
+            return 0;
+        }
+    }
+}
